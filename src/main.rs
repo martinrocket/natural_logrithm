c2cf6faed885c5a5bc7130ca9bf310d6ae511202
martinrocket <martin.ruble@gmail.com>
2025-07-10 05:20:02 +0000

    calculated eulers constant

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("\nThis program wil use Eulers Constant and Natural Logrithms");
 
     let x: f64 = std::f64::consts::E;
-    println!("\nEulers constant is {}.", x); 
+    find_euler();
 
     println!("Natural Log = log(e)x = ln x.");
 
@@ -15,6 +15,8 @@ fn main() {
     println!("log(e)e = {}. ln e = {}.", y, y);
   
     print_ln(15.0);
+
+    
     
     
 }
@@ -24,5 +26,24 @@ fn print_ln(x: f64) {
     let a: f64 = x.clone();
     let b: f64 = a.log(euler);
     println!("log(e){} = {}. ln 10 = {}.\n", a, b, b);
+
+}
+
+fn find_euler(){
+    println!("\nEulers Constant is (1 + x) raised to the power of 1/x.");
+    println!("Eulers constant calculated by Rust is {}.", std::f64::consts::E);
+    // (1 + x) raised to the power of 1/x.  
+    let mut x = 1.0;
+    let mut y: f64 = 0.0;
+   
+    for i in 1..54 {
+        y += y.clone();
+        y = (1.0_f64 + x).powf(1.0_f64/x);
+        x = x/2.0;
+        
+        
+    }
+    println!("Eulers constant calculated by code is {}.", y);
+
 
 }
