d29c2fc91a12213f456582ccccc17c3a8cb1e331
martinrocket <martin.ruble@gmail.com>
2025-07-11 15:54:52 +0000

    added user input to find natural log of y

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
-
-
 use clearscreen::clear;
+use std::io;
+
 fn main() {
+    //clear the console
     clearscreen::clear().expect("failed to clear screen");
 
     println!("\nThis program wil use Eulers Number and Natural Logrithms");
+    
 
+    // x is the Rust calculation or Euler's Number
     let x: f64 = std::f64::consts::E;
+    
     find_euler();
-
-    println!("Natural Log = log(e)x = ln x.");
-
     let y: f64 = x.log(x);
-    println!("log(e)e = {}. ln e = {}.", y, y);
+    println!("Example: log(e)e = {}. ln e = {}.\n", y, y);
+
+   
+    println!("For the calculation y = ln x, we will solve for y. What is your x?:");
+    let y1: f64 = get_float();
+
+
   
-    print_ln(15.0);
+    print_ln(y1);
 
     
     
@@ -25,12 +32,13 @@ fn print_ln(x: f64) {
     let euler: f64 = std::f64::consts::E;
     let a: f64 = x.clone();
     let b: f64 = a.log(euler);
-    println!("log(e){} = {}. ln 10 = {}.\n", a, b, b);
+    println!("log(e){} = {}. ln {} = {}.\n", a, b, a, b);
 
 }
 
 fn find_euler(){
-    println!("\nEulers Number is (1 + x) raised to the power of 1/x.");
+    //find_euler function calculates Eulers Number with Rust math.
+    println!("\nEulers Number is (1 + x) raised to the power of 1/x. Or (1 + x) ^ (1/x)");
     println!("Eulers Number calculated by Rust is {}.", std::f64::consts::E);
     // (1 + x) raised to the power of 1/x.  
     let mut x = 1.0;
@@ -44,6 +52,19 @@ fn find_euler(){
         
     }
     println!("Eulers Number calculated by code is {}.", y);
+    println!("Natural Log = log(e)x = ln x.");
 
 
+}
+
+fn get_float() -> f64 {
+    let mut my_input = String::new();
+    io::stdin().read_line(&mut my_input).expect("failed to read line.");
+    let x: f64 = make_float(my_input.clone());
+    x
+}
+
+fn make_float(x: String) -> f64 {
+    let y = x.trim().parse().expect("Failed to parse");
+    return y;
 }
